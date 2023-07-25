d113dce0ffd2f7ec3ec40a58ab910c09d80861a3
Michael Healy <mhealy.dev@gmail.com>
2023-07-24 21:23:20 -0700

    feat: Starting dev on models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
